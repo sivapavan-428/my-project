dc2e753088494ec9b7fab78b9082ee3b42baae31
Jai siva pavan Reddy Penumaru <jaisivapavanreddy.p@EIS.LOCAL>
2025-09-10 18:55:14 +0530

    some changes

---
+++ Portfolio/jsPractice.js
@@ -0,0 +1,13 @@
+
+console.log("5" * 2 );
+console.log("5" + 2);
+
+console.log("5" - 2);
+
+
+console.log("Hi" * 2 );
+console.log("Hi" + 2 );
+let arr=["hii","woesv"]
+console.log(arr);
+
+let arr2 = []
